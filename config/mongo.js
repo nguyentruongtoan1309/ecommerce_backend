29403fb28decdbd01c5a73704254c25b699565cb
NGUYEN_TRUONG_TOAN <toannguyentruong1309@gmail.com>
2023-07-04 03:12:09 +0700

    update default db connect string

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongo: {
-    database: process.env.DB_CONNECTION_STRING || 'mongodb://localhost/shop',
+    database: process.env.DB_CONNECTION_STRING || 'mongodb+srv://trinhphuongdev:xjJx9zdpdfLS2JCI@cluster0.xrdmevl.mongodb.net/batch-29-30-database',
     options: {
       useNewUrlParser: true,
     },
